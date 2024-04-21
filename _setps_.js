099e534dc7d07c8cb062df38f4596bb9e0fe3cad
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-04-21 21:15:58 +0600

    added new users

--- _setps_.js
+++ _setps_.js
@@ -2,4 +2,11 @@
  *  1. create a post api on the server side
  *  2. client side send data via post
  *  3. set fetch method inside the fetch options (second parameter)
+ *  4. option will have three things:
+ *  method, headers, body
+ *    headers: 'content-type':'aplication/json'
+ *    don't forget to  send data by JSON.stringify in the body
+ *
+ *  5. On the server side: don't forget to use express.json() middleware
+ *
  **/
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Shabana", email: "shabana@gmail.com" },
@@ -28,6 +29,10 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
   console.log("add user");
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
